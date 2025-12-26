fca3ed5e68765d52d8b940a1321256e4deea1463
Myung-Il <ktr040415@gmail.com>
2025-12-26 23:59:19 +0900

    [remove] sqlite structure

--- db/models/guild.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, BigInteger
-from db.base import Base
-
-class Guild(Base):
-    __tablename__ = "guilds"
-
-    id = Column(Integer, primary_key=True, index=True)
-    guild_id = Column(String, unique=True, index=True) # 디스코드 서버 ID
-    name = Column(String) # 서버 이름
--- db/models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from db.base import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    discord_id = Column(String, unique=True, index=True)
-    username = Column(String)
